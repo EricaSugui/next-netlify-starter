c554e0b4ebdc6ce0b339a17ba1837ef5598d42a6
Erica Daikawa <erica.daikawa@quintoandar.com.br>
2023-08-27 20:51:09 -0300

    chore: edita componentes e detalhes

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,33 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Julia Kombucha</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
-        <Header title="Julia Kombucha" />
+        <Header
+          title="Julia Kombucha"
+          logo="/logo-header.png"
+          alt="Julia Kombucha"
+        />
         <p className="description">
           Gostoso e refrescante.
-    <br/>
-    Valinhos, SP
-<br/>
-    
+          <br />
+          Valinhos, SP
+          <br />
+          Instagram: @juliakombuchareal
+          <br />
+          WhatsApp: (19)98999-4753
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
